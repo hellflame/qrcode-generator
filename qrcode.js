e8a2ea5b1fc0f3a2cdadd07337328cb9e3194bdf
hellflame <hellflamedly@gmail.com>
2017-08-25 17:11:27 +0800

    移位溢出说明

--- qrcode.js
+++ qrcode.js
@@ -75,7 +75,8 @@ javascript:(function(){
   function hash(str){
     var result = 0, tab = [0x1234, 0x2345, 0x3456, 0x4567, 0x5678, 0x789a, 0x89ab, 0x9abc, 0xabcd, 0xbcde, 0xcdef];
     for(var i=0;i<str.length;i++){
-    result = 33 * result + str.charCodeAt(i) ^ (i > 0 ? str.charCodeAt(i-1): 66) + tab[i % tab.length];
+      /* 移位操作发生截断 */
+      result = 33 * result + str.charCodeAt(i) ^ (i > 0 ? str.charCodeAt(i-1): 66) + tab[i % tab.length];
     }
     return Math.abs(result).toString(16)
   }
